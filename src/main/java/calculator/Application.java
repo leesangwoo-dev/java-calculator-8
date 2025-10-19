43db9f81b63284eea3ebea5ebda81a011e5b74ff
lsw1096@gmail.com <lsw1096@gmail.com>
2025-10-20 00:26:01 +0900

    feat: [입력] 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // [입력] 사용자에게 입력 메시지를 출력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // [입력] Console.readLine()을 사용하여 입력 받기
+        String input = Console.readLine();
+        System.out.println(input);
     }
 }
